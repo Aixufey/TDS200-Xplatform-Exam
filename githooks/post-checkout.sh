809a2edf4d73abead6dc9f45bd8b9b6a3ba8fe84
Aixufey <jack_11728@msn.com>
2023-10-28 18:35:31 +0200

    Adding git hooks

---
+++ githooks/post-checkout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the name of the branch we just checked out
+BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+# If we checked out the developer branch, pull the latest changes
+if [ "$BRANCH" == "developer" ]; then
+    echo "Checked out developer branch, pulling latest from origin/developer..."
+    git pull origin developer
+fi
