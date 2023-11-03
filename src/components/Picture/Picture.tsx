2268d9e87b204798b27f2782a4b9fdbb84820f19
Aixufey <jack_11728@msn.com>
2023-11-03 05:50:31 +0100

    Updating toggle favorite and multiple select pictures

--- src/components/Picture/Picture.tsx
+++ src/components/Picture/Picture.tsx
@@ -1,16 +1,18 @@
 import { AntDesign, Entypo } from '@expo/vector-icons';
-import { useEffect, useState } from 'react';
-import { GestureResponderEvent, Image, Pressable, View } from 'react-native';
+import React, { memo, useEffect, useState } from 'react';
+import { GestureResponderEvent, Image, Pressable, Text, View } from 'react-native';
 import { useGalleryContext } from '../../context';
 import DesignSystem from '../../styles';
+
 interface IPicture {
     uri?: string;
+    id?: string;
     firstName?: string;
 }
-const Picture: React.FC<IPicture> = ({ uri, firstName }) => {
+const Picture: React.FC<IPicture> = memo(({ uri, id, firstName }) => {
     const [select, setSelect] = useState<boolean>(false);
     const { Colors } = DesignSystem();
-    const fallBackUri = '../../../assets/images/cicada.png';
+    const fallback = require('../../../assets/images/cicada.png');
     const { handleLongPress, isLongPress, setCurrentPicture } = useGalleryContext();
 
     useEffect(() => {
@@ -26,29 +28,41 @@ const Picture: React.FC<IPicture> = ({ uri, firstName }) => {
         console.log(`open`, e.nativeEvent.target);
         if (isLongPress) {
             setSelect((prev) => !prev);
-            setCurrentPicture(firstName);
+            setCurrentPicture({ id: id, firstName: firstName });
         }
     };
 
     return (
-        <Pressable onPress={handlePressPicture} onLongPress={handleLongPress}>
-            <View className="min-w-[75px] min-h-[75px] border-[0.5px] border-dark">
-                <Image
-                    source={require('../../../assets/images/cicada.png')}
-                    className="absolute flex-1 w-full h-full bg-neutral300 "
-                />
-                {isLongPress &&
-                    (!select ? (
-                        <Entypo name="circle" size={12} style={{ color: Colors.secondary }} />
-                    ) : (
-                        <AntDesign
-                            name="checkcircle"
-                            size={12}
-                            style={{ color: Colors.secondary }}
+        <View id="Picture-Container" className="justify-center items-center p-[6px]">
+            <View id="Picture-Body" className="max-w-[75px] max-h-[75px] overflow-hidden">
+                <Pressable onPress={handlePressPicture} onLongPress={handleLongPress}>
+                    <View
+                        id="Picture-Content"
+                        className="min-w-[75px] min-h-[75px] border-[0.5px] border-dark"
+                    >
+                        <Image
+                            source={uri ? { uri } : fallback}
+                            className="absolute flex-1 w-full h-full bg-neutral300"
                         />
-                    ))}
+                        {isLongPress &&
+                            (!select ? (
+                                <Entypo
+                                    name="circle"
+                                    size={12}
+                                    style={{ color: Colors.secondary }}
+                                />
+                            ) : (
+                                <AntDesign
+                                    name="checkcircle"
+                                    size={12}
+                                    style={{ color: Colors.secondary }}
+                                />
+                            ))}
+                        <Text>{firstName}</Text>
+                    </View>
+                </Pressable>
             </View>
-        </Pressable>
+        </View>
     );
-};
+});
 export default Picture;
--- src/screens/Gallery/GalleryScreen.tsx
+++ src/screens/Gallery/GalleryScreen.tsx
@@ -1,30 +1,33 @@
 import { useIsFocused } from '@react-navigation/native';
 import React, { useLayoutEffect } from 'react';
 import { FlatList, View } from 'react-native';
-import DATA from '../../../MOCK_DATA.json';
-import { Background, Canvas, MemoizedItem } from '../../components';
+import { Background, Canvas, Picture } from '../../components';
 import BottomDrawer from '../../components/BottomDrawer';
 import { useGalleryContext } from '../../context';
 
 const GalleryScreen: React.FC = () => {
     const isFocused = useIsFocused();
-    const { showBottomDrawer, resetState } = useGalleryContext();
+    const { showBottomDrawer, resetState, data } = useGalleryContext();
 
     useLayoutEffect(() => {
         return () => {
             // Reset states when leaving the screen
-            // Reset drawer state, press state, long press state
+            // Reset drawer state, press state, long press state, all selected pictures
             resetState();
+            // console.log('Unmounted Gallery Screen');
         };
     }, [isFocused]);
+
     return (
         <Background>
             <View className="flex-1 w-full bottom-[4.5%] justify-center items-center">
                 <Canvas isFocused={isFocused} title={'Media'}>
                     <FlatList
-                        data={DATA}
+                        data={data}
                         keyExtractor={(item) => item.id.toString()}
-                        renderItem={(item) => <MemoizedItem firstName={item.item.first_name} />}
+                        renderItem={({ item, index }) => (
+                            <Picture id={item.id.toString()} firstName={item.first_name} />
+                        )}
                         removeClippedSubviews={true}
                         showsVerticalScrollIndicator={false}
                         initialNumToRender={10}
@@ -35,8 +38,8 @@ const GalleryScreen: React.FC = () => {
                 </Canvas>
             </View>
             <View className="absolute bottom-[50px] h-[10%] w-[100%]">
-                {showBottomDrawer && <BottomDrawer />}
-                {/* <BottomDrawer /> */}
+                {/* {showBottomDrawer && <BottomDrawer />} */}
+                <BottomDrawer />
             </View>
         </Background>
     );
