0e634886c4af0bbfb120a5cbaea94f9f2c2077f4
Aixufey <jack_11728@msn.com>
2023-12-06 22:17:40 +0100

    Can like/dislike, currently with no user

--- src/components/Modal/CustomModal.tsx
+++ src/components/Modal/CustomModal.tsx
@@ -1,8 +1,18 @@
 import { AntDesign, Feather, FontAwesome5 } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
-import { useRef, useState } from 'react';
-import { Image, NativeSyntheticEvent, Text, TextInput, TextInputChangeEventData, TouchableOpacity, View } from 'react-native';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import {
+    Image,
+    NativeSyntheticEvent,
+    Text,
+    TextInput,
+    TextInputChangeEventData,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 import { useGalleryContext, useUIContext } from '../../context';
+import { useFireBase } from '../../context/FireBaseContext.tsx';
 import DesignSystem from '../../styles';
 import { IconButton } from '../Button';
 import BackView from './BackView';
@@ -24,7 +34,32 @@ const CustomModal: React.FC<ICustomModal> = ({
     const { currentPicture } = useGalleryContext();
     const { resetUIState } = useUIContext();
     const [flip, setFlip] = useState<boolean>(false);
-    const [comment, setComment] = useState<string>("");
+    const [comment, setComment] = useState<string>('');
+    const { firebase_db } = useFireBase();
+    const [fetch, setFetch] = useState<boolean>(false);
+    const [feedback, setFeedback] = useState({
+        likes: 0,
+        dislikes: 0,
+    });
+
+    useEffect(() => {
+        const fetchReaction = async () => {
+            if (currentPicture?.id === undefined) return;
+            const pictureRef = doc(firebase_db, 'reactions', currentPicture.id);
+            const reactionDoc = await getDoc(pictureRef);
+
+            setFeedback((prev) => {
+                // console.log('prev like', prev.likes);
+                // console.log('prev dislike', prev.dislikes);
+                return {
+                    likes: (prev.likes = reactionDoc.data()?.likes),
+                    dislikes: (prev.dislikes = reactionDoc.data()?.dislikes),
+                };
+            });
+            setFetch(false);
+        };
+        fetchReaction();
+    }, [currentPicture, fetch]);
 
     const handleOnPressClose = () => {
         onPress && onPress();
@@ -35,13 +70,53 @@ const CustomModal: React.FC<ICustomModal> = ({
     const handleFlip = () => {
         setFlip((prev) => !prev);
     };
-    
+
     const handleCommentChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
         setComment(e.nativeEvent.text);
-    }
+    };
     const handleSubmitPress = () => {
-        console.log(comment)
-    }
+        console.log(comment);
+    };
+
+    const handleLike = () => {
+        updateFirestoreReaction(true);
+    };
+    const handleDislike = () => {
+        updateFirestoreReaction(false);
+    };
+
+    const updateFirestoreReaction = async (state: boolean) => {
+        if (currentPicture?.id !== undefined) {
+            const pictureRef = doc(firebase_db, 'reactions', currentPicture.id);
+
+            const reactionDoc = await getDoc(pictureRef);
+            setFeedback((prev) => {
+                return {
+                    likes: (prev.likes = reactionDoc.data()?.likes),
+                    dislikes: (prev.dislikes = reactionDoc.data()?.dislikes),
+                };
+            });
+
+            // user can either like or dislike
+            let like = state ? 1 : -1;
+            let dislike = state ? -1 : 1;
+
+            // guard not below 0
+            like = like < 0 ? 0 : like;
+            dislike = dislike < 0 ? 0 : dislike;
+            await setDoc(
+                pictureRef,
+                {
+                    likes: like,
+                    dislikes: dislike,
+                    pictureId: currentPicture.id,
+                    userId: '123',
+                },
+                { merge: true }
+            );
+            setFetch(true);
+        }
+    };
 
     return (
         toggleModal && (
@@ -60,20 +135,36 @@ const CustomModal: React.FC<ICustomModal> = ({
                             </View>
                             <View className="w-[250px] h-[50px] p-1 items-center flex-row">
                                 <View className="px-2 flex-row">
-                                    <AntDesign name="like2" size={24} color={Colors.tertiary} />
-                                    <Text className="px-1 text-neutral font-handjet-light">1</Text>
+                                    <TouchableOpacity onPress={handleLike}>
+                                        <AntDesign name="like2" size={24} color={Colors.tertiary} />
+                                    </TouchableOpacity>
+                                    <Text className="px-1 text-neutral font-handjet-light">
+                                        {feedback.likes}
+                                    </Text>
                                 </View>
                                 <View className="px-2 flex-row">
-                                    <AntDesign name="dislike2" size={24} color={Colors.tertiary} />
-                                    <Text className="px-1 text-neutral font-handjet-light">0</Text>
+                                    <TouchableOpacity onPress={handleDislike}>
+                                        <AntDesign
+                                            name="dislike2"
+                                            size={24}
+                                            color={Colors.tertiary}
+                                        />
+                                    </TouchableOpacity>
+                                    <Text className="px-1 text-neutral font-handjet-light">
+                                        {feedback.dislikes}
+                                    </Text>
                                 </View>
-                                <View className='px-2 flex-row'>
-                                    <FontAwesome5 name="comment-alt" size={24} color={Colors.tertiary} />
+                                <View className="px-2 flex-row">
+                                    <FontAwesome5
+                                        name="comment-alt"
+                                        size={24}
+                                        color={Colors.tertiary}
+                                    />
                                 </View>
                             </View>
                             <View>
                                 <TouchableOpacity>
-                                    <Text className='px-1 text-neutral50'>View all comments</Text>
+                                    <Text className="px-1 text-neutral50">View all comments</Text>
                                 </TouchableOpacity>
                             </View>
                             <View className="w-[250px] h-[60px] overflow-hidden flex-row">
@@ -85,13 +176,17 @@ const CustomModal: React.FC<ICustomModal> = ({
                                     placeholderTextColor={Colors.neutral700}
                                     className="px-1 text-neutral font-handjet-light w-[220px]"
                                 />
-                                {comment.length >= 1 &&
+                                {comment.length >= 1 && (
                                     <View className="w-100 justify-center items-center">
                                         <TouchableOpacity onPress={handleSubmitPress}>
-                                            <Feather name="send" size={24} color={Colors.tertiary} />
+                                            <Feather
+                                                name="send"
+                                                size={24}
+                                                color={Colors.tertiary}
+                                            />
                                         </TouchableOpacity>
                                     </View>
-                                }
+                                )}
                             </View>
                         </View>
                     )}
