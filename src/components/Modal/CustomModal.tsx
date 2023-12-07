83adb5b758b304d511066f2a5f7225dabbc22c0c
Aixufey <jack_11728@msn.com>
2023-12-07 03:08:45 +0100

    Can add comment, testing with dummy

--- src/components/Modal/CustomModal.tsx
+++ src/components/Modal/CustomModal.tsx
@@ -1,8 +1,10 @@
 import { AntDesign, Feather, FontAwesome5 } from '@expo/vector-icons';
 import { BlurView } from 'expo-blur';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { DocumentData, addDoc, collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
+import dummyComments from '../../test/DummyComment';
 import { useEffect, useState } from 'react';
 import {
+    FlatList,
     Image,
     NativeSyntheticEvent,
     Text,
@@ -11,6 +13,7 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native';
+import { commentsDoc, reactionsDoc } from '../../constants';
 import { useGalleryContext, useUIContext } from '../../context';
 import { useFireBase } from '../../context/FireBaseContext.tsx';
 import DesignSystem from '../../styles';
@@ -31,21 +34,23 @@ const CustomModal: React.FC<ICustomModal> = ({
     children,
 }) => {
     const { Colors } = DesignSystem();
+    const { firebase_db } = useFireBase();
     const { currentPicture } = useGalleryContext();
     const { resetUIState } = useUIContext();
-    const [flip, setFlip] = useState<boolean>(false);
-    const [comment, setComment] = useState<string>('');
-    const { firebase_db } = useFireBase();
     const [fetch, setFetch] = useState<boolean>(false);
+    const [flip, setFlip] = useState<boolean>(false);
+    const [showComment, setShowComment] = useState<boolean>(false);
+    const [input, setInput] = useState<string>('');
+    const [comments, setComments] = useState<DocumentData[]>([]);
     const [feedback, setFeedback] = useState({
         likes: 0,
         dislikes: 0,
     });
 
     useEffect(() => {
-        const fetchReaction = async () => {
+        const fetchReactions = async () => {
             if (currentPicture?.id === undefined) return;
-            const pictureRef = doc(firebase_db, 'reactions', currentPicture.id);
+            const pictureRef = doc(firebase_db, reactionsDoc, currentPicture.id);
             const reactionDoc = await getDoc(pictureRef);
 
             setFeedback((prev) => {
@@ -58,7 +63,20 @@ const CustomModal: React.FC<ICustomModal> = ({
             });
             setFetch(false);
         };
-        fetchReaction();
+        const fetchComments = async () => {
+            if (currentPicture?.id === undefined) return;
+            const pictureRef = doc(firebase_db, commentsDoc, currentPicture.id);
+            const commentsCollection = collection(pictureRef, commentsDoc);
+            const commentDoc = await getDocs(commentsCollection);
+            const comments = commentDoc.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            console.log(comments);
+            setComments(comments);
+        };
+        //fetchReactions();
+        // fetchComments();
     }, [currentPicture, fetch]);
 
     const handleOnPressClose = () => {
@@ -72,10 +90,17 @@ const CustomModal: React.FC<ICustomModal> = ({
     };
 
     const handleCommentChange = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
-        setComment(e.nativeEvent.text);
+        setInput(e.nativeEvent.text);
+    };
+    const handleShowCommentPress = () => {
+        setShowComment((prev) => !prev);
+        console.log(showComment);
     };
     const handleSubmitPress = () => {
-        console.log(comment);
+        if (currentPicture?.id === undefined) {
+            return alert('Picture does not exist!');
+        }
+        updateFirestoreComments(currentPicture?.id, 'User333222111');
     };
 
     const handleLike = () => {
@@ -87,7 +112,7 @@ const CustomModal: React.FC<ICustomModal> = ({
 
     const updateFirestoreReaction = async (state: boolean) => {
         if (currentPicture?.id !== undefined) {
-            const pictureRef = doc(firebase_db, 'reactions', currentPicture.id);
+            const pictureRef = doc(firebase_db, reactionsDoc, currentPicture.id);
 
             const reactionDoc = await getDoc(pictureRef);
             setFeedback((prev) => {
@@ -110,117 +135,172 @@ const CustomModal: React.FC<ICustomModal> = ({
                     likes: like,
                     dislikes: dislike,
                     pictureId: currentPicture.id,
-                    userId: '123',
+                    userId: 'User123',
                 },
                 { merge: true }
             );
             setFetch(true);
         }
     };
-
+    const updateFirestoreComments = async (pictureId: string, userId: string) => {
+        console.log(input);
+        const pictureRef = doc(firebase_db, commentsDoc, pictureId);
+        const commentCol = collection(pictureRef, commentsDoc);
+        await addDoc(commentCol, {
+            userId: userId,
+            pictureId: pictureId,
+            comment: input,
+            timeStamp: new Date(),
+        });
+        setInput('');
+    };
     return (
         toggleModal && (
             <BlurView intensity={intensity || 5} tint="dark" className={className}>
-                <View className="absolute top-15 w-[75%] h-[460px] border-[1px] border-[#00ffff] bg-dark300 rounded-2xl overflow-hidden justify-center items-center">
-                    <View className="w-full h-full justify-start items-center absolute">
-                        {children}
-                    </View>
-                    {currentPicture && (
-                        <View className="overflow-hidden justify-start h-[360px]">
-                            <View className="overflow-hidden w-[250px] h-[250px] rounded-md">
-                                <Image
-                                    className="w-full h-full"
-                                    source={{ uri: currentPicture?.uri }}
+                {showComment ? (
+                    <View className="flex absolute top-15 w-[75%] h-[460px] border-[1px] border-[#00ffff] bg-dark300 rounded-2xl overflow-hidden justify-center items-center">
+                        <View className="w-full h-full justify-start items-center absolute">
+                            <Text className="text-neutral font-handjet-light text-xl">
+                                Comments
+                            </Text>
+                        </View>
+                        <View className="basis-[10%] px-2 w-full h-full flex-row justify-end items-center">
+                            <TouchableOpacity className="absolute w-[20%] h-full flex-row justify-center items-center">
+                                <IconButton
+                                    onPress={() => setShowComment(false)}
+                                    className="w-[45px] h-[45px] justify-center items-center"
+                                    IconSet="AntDesign"
+                                    iconName="closecircle"
+                                    iconSize={24}
+                                    iconColor={Colors.tertiary}
                                 />
-                            </View>
-                            <View className="w-[250px] h-[50px] p-1 items-center flex-row">
-                                <View className="px-2 flex-row">
-                                    <TouchableOpacity onPress={handleLike}>
-                                        <AntDesign name="like2" size={24} color={Colors.tertiary} />
-                                    </TouchableOpacity>
-                                    <Text className="px-1 text-neutral font-handjet-light">
-                                        {feedback.likes}
+                            </TouchableOpacity>
+                        </View>
+                        <FlatList
+                            className="basis-3/4 w-full h-64 border-2 border-[#Faa]"
+                            data={dummyComments}
+                            keyExtractor={(item: DocumentData) => item.id}
+                            renderItem={({ item }) => (
+                                <View className="p-2">
+                                    <View className="flex-row">
+                                        <Text className="text-neutral font-handjet-light">
+                                            <Text className="text-secondary font-handjet-black tracking-widest">
+                                                {item.userId}
+                                            </Text>
+                                            : {item.comment}
+                                        </Text>
+                                    </View>
+                                    <Text className="text-neutral font-handjet-light">
+                                        {item.timeStamp.toUTCString()}
                                     </Text>
                                 </View>
-                                <View className="px-2 flex-row">
-                                    <TouchableOpacity onPress={handleDislike}>
-                                        <AntDesign
-                                            name="dislike2"
-                                            size={24}
-                                            color={Colors.tertiary}
-                                        />
-                                    </TouchableOpacity>
-                                    <Text className="px-1 text-neutral font-handjet-light">
-                                        {feedback.dislikes}
-                                    </Text>
-                                </View>
-                                <View className="px-2 flex-row">
-                                    <FontAwesome5
-                                        name="comment-alt"
-                                        size={24}
-                                        color={Colors.tertiary}
+                            )}
+                        />
+                    </View>
+                ) : (
+                    <View className="absolute top-15 w-[75%] h-[460px] border-[1px] border-[#00ffff] bg-dark300 rounded-2xl overflow-hidden justify-center items-center">
+                        <View className="w-full h-full justify-start items-center absolute">
+                            {children}
+                        </View>
+                        {currentPicture && (
+                            <View className="overflow-hidden justify-center h-[360px]">
+                                <View className="overflow-hidden w-[250px] h-[250px] rounded-md">
+                                    <Image
+                                        className="w-full h-full"
+                                        source={{ uri: currentPicture?.uri }}
                                     />
                                 </View>
-                            </View>
-                            <View>
-                                <TouchableOpacity>
-                                    <Text className="px-1 text-neutral50">View all comments</Text>
-                                </TouchableOpacity>
-                            </View>
-                            <View className="w-[250px] h-[60px] overflow-hidden flex-row">
-                                <TextInput
-                                    onChange={handleCommentChange}
-                                    multiline={true}
-                                    numberOfLines={10}
-                                    placeholder="Write a comment..."
-                                    placeholderTextColor={Colors.neutral700}
-                                    className="px-1 text-neutral font-handjet-light w-[220px]"
-                                />
-                                {comment.length >= 1 && (
-                                    <View className="w-100 justify-center items-center">
-                                        <TouchableOpacity onPress={handleSubmitPress}>
-                                            <Feather
-                                                name="send"
+                                <View className="w-[250px] h-[50px] p-1 items-center flex-row">
+                                    <View className="px-2 flex-row">
+                                        <TouchableOpacity onPress={handleLike}>
+                                            <AntDesign
+                                                name="like2"
+                                                size={24}
+                                                color={Colors.tertiary}
+                                            />
+                                        </TouchableOpacity>
+                                        <Text className="px-1 text-neutral font-handjet-light">
+                                            {feedback.likes}
+                                        </Text>
+                                    </View>
+                                    <View className="px-2 flex-row">
+                                        <TouchableOpacity onPress={handleDislike}>
+                                            <AntDesign
+                                                name="dislike2"
+                                                size={24}
+                                                color={Colors.tertiary}
+                                            />
+                                        </TouchableOpacity>
+                                        <Text className="px-1 text-neutral font-handjet-light">
+                                            {feedback.dislikes}
+                                        </Text>
+                                    </View>
+                                    <View className="px-2 flex-row">
+                                        <TouchableOpacity onPress={() => setShowComment(true)}>
+                                            <FontAwesome5
+                                                name="comment-alt"
                                                 size={24}
                                                 color={Colors.tertiary}
                                             />
                                         </TouchableOpacity>
                                     </View>
-                                )}
+                                </View>
+                                
+                                <View className="w-[250px] h-[60px] overflow-hidden flex-row">
+                                    <TextInput
+                                        onChange={handleCommentChange}
+                                        multiline={true}
+                                        numberOfLines={10}
+                                        placeholder="Write a comment..."
+                                        placeholderTextColor={Colors.neutral700}
+                                        className="px-1 text-neutral font-handjet-light w-[220px]"
+                                    />
+                                    {input.length >= 1 && (
+                                        <View className="w-100 justify-center items-center">
+                                            <TouchableOpacity onPress={handleSubmitPress}>
+                                                <Feather
+                                                    name="send"
+                                                    size={24}
+                                                    color={Colors.tertiary}
+                                                />
+                                            </TouchableOpacity>
+                                        </View>
+                                    )}
+                                </View>
                             </View>
+                        )}
+
+                        {flip && currentPicture && (
+                            <BackView
+                                id={currentPicture.id}
+                                uri={currentPicture.uri}
+                                coordinates={{
+                                    longitude: currentPicture.coordinates?.longitude,
+                                    latitude: currentPicture.coordinates?.latitude,
+                                }}
+                            />
+                        )}
+
+                        <View className="absolute top-0 right-0 h-[15%] flex-row justify-end items-center">
+                            <IconButton
+                                onPress={handleFlip}
+                                className="w-[45px] h-[45px] justify-center items-center"
+                                IconSet="MaterialIcons"
+                                iconName="flip"
+                                iconSize={28}
+                                iconColor={Colors.tertiary}
+                            />
+                            <IconButton
+                                onPress={handleOnPressClose}
+                                className="w-[45px] h-[45px] justify-center items-center"
+                                IconSet="AntDesign"
+                                iconName="closecircle"
+                                iconSize={24}
+                                iconColor={Colors.tertiary}
+                            />
                         </View>
-                    )}
-
-                    {flip && currentPicture && (
-                        <BackView
-                            id={currentPicture.id}
-                            uri={currentPicture.uri}
-                            coordinates={{
-                                longitude: currentPicture.coordinates?.longitude,
-                                latitude: currentPicture.coordinates?.latitude,
-                            }}
-                        />
-                    )}
-
-                    <View className="absolute top-0 right-0 h-[15%] flex-row justify-end items-center">
-                        <IconButton
-                            onPress={handleFlip}
-                            className="w-[45px] h-[45px] justify-center items-center"
-                            IconSet="MaterialIcons"
-                            iconName="flip"
-                            iconSize={28}
-                            iconColor={Colors.tertiary}
-                        />
-                        <IconButton
-                            onPress={handleOnPressClose}
-                            className="w-[45px] h-[45px] justify-center items-center"
-                            IconSet="AntDesign"
-                            iconName="closecircle"
-                            iconSize={24}
-                            iconColor={Colors.tertiary}
-                        />
                     </View>
-                </View>
+                )}
             </BlurView>
         )
     );
