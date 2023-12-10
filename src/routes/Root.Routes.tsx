bde3362818b48bb28a6f7c8f70be39347646a43c
Aixufey <jack_11728@msn.com>
2023-12-10 16:46:06 +0100

    Separate guest view

--- src/routes/Root.Routes.tsx
+++ src/routes/Root.Routes.tsx
@@ -1,7 +1,7 @@
 import { AntDesign, Entypo, MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text } from 'react-native';
-import { useGalleryContext, useUIContext } from '../context';
+import { useAuth, useGalleryContext, useUIContext } from '../context';
 import { useCustomNavigation } from '../hooks';
 import { CameraScreen, GalleryScreen, HomeScreen } from '../screens';
 import DesignSystem from '../styles';
@@ -11,6 +11,7 @@ const RootRoutes: React.FC = () => {
         useGalleryContext();
     const { isLongPressMenu, resetUIState } = useUIContext();
     const { Colors } = DesignSystem();
+    const { currentUser } = useAuth();
     const { goBack } = useCustomNavigation();
 
     return (
@@ -34,111 +35,142 @@ const RootRoutes: React.FC = () => {
                 ],
             }}
         >
-            <Tab.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ focused }) =>
-                        !isLongPressMenu ? (
-                            <Entypo
-                                name="home"
-                                size={24}
-                                color={focused ? Colors.secondary : Colors.tertiaryRei}
-                                style={{ bottom: focused ? 5 : 0 }}
-                            />
-                        ) : (
-                            <MaterialIcons
-                                onPress={() => (resetGalleryState(), resetUIState())}
-                                name="arrow-back-ios"
-                                size={24}
-                                color={Colors.neutral}
-                            />
-                        ),
-                    tabBarLabel: ({ focused }) => (
-                        <Text
-                            className={`font-handjet-regular tracking-widest ${
-                                focused ? 'text-secondary' : 'text-tertiaryRei'
-                            } ${focused ? 'bottom-1' : 'bottom-0'}`}
-                        >
-                            {!isLongPressMenu && 'Home'}
-                        </Text>
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Gallery"
-                component={GalleryScreen}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ focused }) =>
-                        !isLongPressMenu ? (
-                            <Entypo
-                                name="image"
-                                size={24}
-                                color={focused ? Colors.secondary : Colors.tertiaryRei}
-                                style={{ bottom: focused ? 5 : 0 }}
-                            />
-                        ) : (
-                            <MaterialIcons
-                                onPress={() => toggleFavorite(selectedPictures)}
-                                name="favorite-border"
-                                size={24}
-                                color={Colors.neutral}
-                            />
-                        ),
-                    tabBarLabel: ({ focused }) => (
-                        <Text
-                            className={`font-handjet-regular tracking-widest ${
-                                focused ? 'text-secondary' : 'text-tertiaryRei'
-                            } ${focused ? 'bottom-1' : 'bottom-0'}`}
-                        >
-                            {!isLongPressMenu && 'Gallery'}
-                        </Text>
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Camera"
-                component={CameraScreen}
-                listeners={({ navigation, route }) => ({
-                    tabPress: (e) => {
-                        isLongPressMenu ? e.preventDefault() : navigation.navigate('Camera');
-                    },
-                })}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ focused }) =>
-                        !isLongPressMenu ? (
-                            <Entypo
-                                name="camera"
-                                size={24}
-                                color={focused ? Colors.secondary : Colors.tertiaryRei}
-                                style={{ bottom: focused ? 5 : 0 }}
-                            />
-                        ) : (
-                            <AntDesign
-                                onPress={() =>
-                                    selectedPictures && selectedPictures.length > 0
-                                        ? handleDeletePicture(selectedPictures)
-                                        : null
-                                }
-                                name="delete"
-                                size={24}
-                                color={Colors.neutral}
-                            />
-                        ),
-                    tabBarLabel: ({ focused }) => (
-                        <Text
-                            className={`font-handjet-regular tracking-widest ${
-                                focused ? 'text-secondary' : 'text-tertiaryRei'
-                            } ${focused ? 'bottom-1' : 'bottom-0'}`}
-                        >
-                            {!isLongPressMenu && 'Camera'}
-                        </Text>
-                    ),
-                }}
-            />
+            {currentUser && currentUser?.isAnonymous ? (
+                <>
+                    <Tab.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: () => (
+                                <Entypo
+                                    name="home"
+                                    size={24}
+                                    color={Colors.secondary}
+                                    style={{ bottom: 0 }}
+                                />
+                            ),
+                            tabBarLabel: () => (
+                                <Text
+                                    className={`font-handjet-regular tracking-widest text-secondary bottom-0`}
+                                >
+                                    Home
+                                </Text>
+                            ),
+                        }}
+                    />
+                </>
+            ) : (
+                <>
+                    <Tab.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: ({ focused }) =>
+                                !isLongPressMenu ? (
+                                    <Entypo
+                                        name="home"
+                                        size={24}
+                                        color={focused ? Colors.secondary : Colors.tertiaryRei}
+                                        style={{ bottom: focused ? 5 : 0 }}
+                                    />
+                                ) : (
+                                    <MaterialIcons
+                                        onPress={() => (resetGalleryState(), resetUIState())}
+                                        name="arrow-back-ios"
+                                        size={24}
+                                        color={Colors.neutral}
+                                    />
+                                ),
+                            tabBarLabel: ({ focused }) => (
+                                <Text
+                                    className={`font-handjet-regular tracking-widest ${
+                                        focused ? 'text-secondary' : 'text-tertiaryRei'
+                                    } ${focused ? 'bottom-1' : 'bottom-0'}`}
+                                >
+                                    {!isLongPressMenu && 'Home'}
+                                </Text>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Gallery"
+                        component={GalleryScreen}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: ({ focused }) =>
+                                !isLongPressMenu ? (
+                                    <Entypo
+                                        name="image"
+                                        size={24}
+                                        color={focused ? Colors.secondary : Colors.tertiaryRei}
+                                        style={{ bottom: focused ? 5 : 0 }}
+                                    />
+                                ) : (
+                                    <MaterialIcons
+                                        onPress={() => toggleFavorite(selectedPictures)}
+                                        name="favorite-border"
+                                        size={24}
+                                        color={Colors.neutral}
+                                    />
+                                ),
+                            tabBarLabel: ({ focused }) => (
+                                <Text
+                                    className={`font-handjet-regular tracking-widest ${
+                                        focused ? 'text-secondary' : 'text-tertiaryRei'
+                                    } ${focused ? 'bottom-1' : 'bottom-0'}`}
+                                >
+                                    {!isLongPressMenu && 'Gallery'}
+                                </Text>
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Camera"
+                        component={CameraScreen}
+                        listeners={({ navigation, route }) => ({
+                            tabPress: (e) => {
+                                isLongPressMenu
+                                    ? e.preventDefault()
+                                    : navigation.navigate('Camera');
+                            },
+                        })}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: ({ focused }) =>
+                                !isLongPressMenu ? (
+                                    <Entypo
+                                        name="camera"
+                                        size={24}
+                                        color={focused ? Colors.secondary : Colors.tertiaryRei}
+                                        style={{ bottom: focused ? 5 : 0 }}
+                                    />
+                                ) : (
+                                    <AntDesign
+                                        onPress={() =>
+                                            selectedPictures && selectedPictures.length > 0
+                                                ? handleDeletePicture(selectedPictures)
+                                                : null
+                                        }
+                                        name="delete"
+                                        size={24}
+                                        color={Colors.neutral}
+                                    />
+                                ),
+                            tabBarLabel: ({ focused }) => (
+                                <Text
+                                    className={`font-handjet-regular tracking-widest ${
+                                        focused ? 'text-secondary' : 'text-tertiaryRei'
+                                    } ${focused ? 'bottom-1' : 'bottom-0'}`}
+                                >
+                                    {!isLongPressMenu && 'Camera'}
+                                </Text>
+                            ),
+                        }}
+                    />
+                </>
+            )}
         </Tab.Navigator>
     );
 };
