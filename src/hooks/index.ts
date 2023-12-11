f76c604ba456654cbb1338a2ee8d57e5720abc8e
Aixufey <jack_11728@msn.com>
2023-12-11 23:26:33 +0100

    Can upload img from lib to firebase

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export * from './useCustomFonts';
 export * from './useCustomNavigation';
 export * from './useFetchAlbum';
 export * from './useFetchBucketList';
+export * from './useImagePicker';
 export * from './useLaunchCamera';
 export * from './useManipulateImage';
 export * from './useRequestPermission';
---
+++ src/hooks/useImagePicker/index.ts
@@ -0,0 +1,2 @@
+import useImagePicker from './useImagePicker';
+export { useImagePicker };
